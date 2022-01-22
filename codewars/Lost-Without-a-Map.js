f7f454c3ef67efc7a06ee0de5db5cc3adf6e7d27
Mykola <nicholaskaradzha@gmail.com>
2022-01-22 12:18:16 +0700

    new kata solved

--- codewars/Lost-Without-a-Map.js
+++ codewars/Lost-Without-a-Map.js
@@ -1,5 +1,14 @@
-function findNeedle(haystack) {
-    for (let i in haystack) {
-        if (haystack[i] === "needle") {return `found the needle at position ${i}`}
+const cannonsReady = (gunners) => {
+    let count = 0
+    for (let prop in gunners) {
+        if (gunners[prop] === "aye") {
+            count = 1;
+        }
+        else {
+            return "Shiver me timbers!";
+        }
+    }
+    if (count == 1) {
+        return "Fire!"
     }
 }
